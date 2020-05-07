2a2860d6b318a5f9eb85686fe3869f7e1cd21627
Sun Chong <sun.chong@msn.com>
2020-05-07 15:44:50 +0800

    初始化提交macOS脚本

---
+++ README.md
@@ -0,0 +1,30 @@
+# macOS系统初始化脚本
+
+![macOS Init](https://img.shields.io/badge/macOS-Init-brightgreen)
+
+个人自用的macOS系统初始化脚本。
+
+主要用于折腾，其次用来更新记录基础hosts、基础开发环境配置、zsh配置以及自用软件清单等，再次用来初始化macOS系统。
+
+## 使用脚本
+
+```bash
+cd 脚本所在目录
+chmod +x macOS-Init.sh #添加执行权限
+./macOS-Init.sh #执行
+```
+
+## res文件
+
+- [hosts](res/hosts)：备份基础hosts文件，用于追加到系统`hosts`。更完善的hosts可参考[googlehosts/hosts](https://github.com/googlehosts/hosts)、[HostsToolforMac](https://github.com/ZzzM/HostsToolforMac)，或通过[IPAddress](https://www.ipaddress.com/)查询。
+- [zshrc](res/zshrc)：备份zsh配置文件，用于覆盖`.zshrc`。更详细的配置可参考[ohmyzsh](https://github.com/ohmyzsh/ohmyzsh)。
+- [iTerm2.json](res/iTerm2.json)：暂仅备份。
+
+## 主要配置内容
+
+- 更改默认的计算机名；
+- 将`res/hosts`中备份的基础hosts配置，追加到系统`hosts`中；
+- 安装Command Line Tools，及安装App Store中推荐的更新；
+- 安装[Homebrew](https://github.com/Homebrew/brew)、brew软件包清单和[brew cask](https://github.com/Homebrew/homebrew-cask/)应用清单；
+- 配置[Oh My Zsh](https://github.com/ohmyzsh/ohmyzsh)，将`res/zshrc`中备份的配置文件覆盖`.zshrc`；
+- 其他配置，pip安装插件及git基础配置。
---
+++ macOS-Init.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# -------------------
+# - macOS个人装机配置 -
+# -------------------
+
+set -e
+# set -x
+
+#获取当前目录
+readonly CUR_DIR="$(cd "$(dirname "$0")" && pwd)/res/"
+
+#自定义信息输出函数
+function cus_echo() {
+    local CL_GREEN="42;30m" #$2 : 绿底黑字
+    local CL_RED="41;37m"   #$2 : 红底白字
+    local MSG=$1            #$1 : 信息内容
+    if [ "$3" = "" ]; then  #$3 : 空:显示时间；fase:隐藏时间
+        MSG="$(date "+%Y-%m-%d %H:%M:%S") -> $1"
+    fi
+    case $2 in
+    g)
+        echo -e "\033[$CL_GREEN$MSG\033[0m"
+        ;;
+    r)
+        echo -e "\033[$CL_RED$MSG\033[0m"
+        ;;
+    *)
+        echo "$MSG"
+        ;;
+    esac
+}
+
+cus_echo "Hello $(whoami), 需要输入密码：" r false
+
+#更改计算机名
+sudo scutil --set LocalHostName sunc-MBP
+cus_echo "已更改计算机名" g
+
+#更新hosts，将自定义hosts追加到系统hosts中
+readonly HOST_NAME="hosts"
+sudo sh -c "cat $CUR_DIR/$HOST_NAME >> /etc/hosts"
+sudo dscacheutil -flushcache
+cus_echo "已更新hosts" g
+
+#安装Command Line Tools
+xcode-select --install
+
+#安装App Store推荐的更新
+sudo softwareupdate -i -r
+cus_echo "已更新App Store应用" g
+
+#安装Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+cus_echo "已安装Homebrew" g
+
+brew install python     #安装Python最新版
+brew install zsh        #安装zsh最新版
+brew install git        #安装git最新版
+brew install shellcheck #安装ShellCheck
+cus_echo "已完成安装brew软件包清单" g
+brew cleanup
+
+#安装Oh My Zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#安装zsh-syntax-highlighting高亮插件
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#安装zsh-autosuggestions自动提示插件
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#更新zsh配置文件（备份配置中已包含环境变量）
+readonly ZSHRC_NAME="zshrc"
+cat "$CUR_DIR/$ZSHRC_NAME" >~/.zshrc
+
+cus_echo "已完成配置Oh My Zsh" g
+
+#安装应用
+brew cask install visual-studio-code #安装vscode
+brew cask install tencent-lemon      #安装Lemon
+brew cask install baiduinput         #安装百度输入法
+brew cask install baidunetdisk       #安装百度网盘
+brew cask install sunlogincontrol    #安装向日葵控制端
+brew cask install iina               #安装IINA视频播放器
+brew cask install itsycal            #安装Itsycal日历
+brew cask install motrix             #安装Motrix下载器
+brew cask install iterm2             #安装iTerm2终端
+# brew cask install google-chrome #安装Chrome
+cus_echo "已完成安装brew cask应用清单" g
+
+#Python插件
+pip3 install flake8
+pip3 install yapf
+
+#Git基础配置
+git config --global color.ui auto
+git config --global color.status auto
+git config --global color.diff auto
+git config --global color.branch auto
+git config --global color.interactive auto
+
+cus_echo "全部运行完毕" r
+say Init is complete
